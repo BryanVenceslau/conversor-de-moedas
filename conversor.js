7ea4f3d2242a88fee15fa8876c0cf27175e393ce
BryanVenceslau <123129659+BryanVenceslau@users.noreply.github.com>
2023-02-04 13:12:01 -0300

    Update conversor.js

--- conversor.js
+++ conversor.js
@@ -1,12 +1,12 @@
 function converter() {
   var cotacaoEmReal = 123800.23;
   var nome = prompt("Digite seu nome");
-  var numero = prompt(
+  var valor = prompt(
     nome +
       ", qual o valor a ser convertido?\nDetalhe, apenas convertemos Bitcoin para Real"
   );
 
-  var resultado = Number(numero) * cotacaoEmReal;
+  var resultado = Number(valor) * cotacaoEmReal;
 
   alert("O valor é de R$ " + resultado);
 }
